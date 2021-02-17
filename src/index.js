2c0299b687a424e62dadf9b91fba12c15251403c
TRUONG TRUNG HIEU <1721050670@student.humg.edu.vn>
2021-02-17 22:02:50 +0700

    up date 1002

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const app = require('./server');
+const listEndPoints = require('express-list-endpoints');
 const { PORT } = require('./configs');
 const port = PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  console.log(listEndPoints(app));
 });
