9bdba46e39bea3a3aff36fca5e0e3501360973c0
TRUONG TRUNG HIEU <1721050670@student.humg.edu.vn>
2021-02-17 19:55:12 +0700

    update

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 const express = require('express');
 const rateLimit = require("express-rate-limit");
+const slowDown = require("express-slow-down");
 
 const app = express();
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 5 // limit each IP to 100 requests per windowMs
+    max: 100 // limit each IP to 100 requests per windowMs
 });
 
+const speedLimiter = slowDown({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  delayAfter: 10, // allow 100 requests per 15 minutes, then...
+  delayMs: 500 // begin adding 500ms of delay per request above 100:
+});
 
-app.use(limiter);
 // TODO: Limit a request if it has exceed 
+app.use(speedLimiter);
+app.use(limiter);
+
 
 app.get("/", (req, res) => {
     res.json({
-        message: "The request is ready ..."
+        message: "The request is ready ...",
+        ip: req.ip
     })
 })
 
